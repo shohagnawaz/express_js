2142582b266cf9d16d25c28b4c8aec7b52aeefa6
shohagnawaz <snawazdigital@gmail.com>
2026-01-13 19:18:03 +0600

    token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,1 +1,10 @@
 // higher order function = reutrn korbe akta function k
+
+import { NextFunction, Request, Response } from "express"
+
+const auth = () => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization;
+        console.log(token)
+    }
+}
